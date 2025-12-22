e15cd2412aba8f67f56fba146aab756000d43879
y2k <itwisterlx@gmail.com>
2025-12-22 16:55:32 +0100

    Update title from version 0.3 to 0.4

--- build/html.clj
+++ build/html.clj
@@ -4,7 +4,7 @@
  [:html
   [:head
    [:meta {:charset "UTF-8"}]
-   [:title "Игра \"Сапер\" (0.3)"]
+   [:title "Игра \"Сапер\" (0.4)"]
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
    [:style ":root { --color-text: #999; --color-tile: #30c9bc; --color-tile-opened: #f8f8f8; --color-bg: #ffffff; } @media (prefers-color-scheme: dark) { :root { --color-text: #999; --color-tile: #2a8278; --color-tile-opened: #202020; --color-bg: #121212; } }"]]
   [:body {:style "background-color: var(--color-bg); user-select: none; -webkit-user-select: none; margin: 0px;"}
